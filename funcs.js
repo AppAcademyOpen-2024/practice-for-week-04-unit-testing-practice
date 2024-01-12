c26896b266831a3e6f22dc35c9f9f72200373781
Dvwhite81 <Dvwhite1981@gmail.com>
2024-01-12 16:51:47 -0500

    complete

--- funcs.js
+++ funcs.js
@@ -1,13 +1,23 @@
 function isFive(num) {
   // Your code here
+  return num === 5;
 }
 
 function isOdd(number) {
   // Your code here
+  if (typeof number !== 'number') {
+    throw Error('must be a number');
+  }
+  return number % 2 !== 0;
 }
 
 function myRange(min, max, step = 1) {
   // Your code here
+  const result = [];
+  for (let i = min; i <= max; i += step) {
+    result.push(i);
+  }
+  return result;
 }
 
 
--- test/class-spec.js
+++ test/class-spec.js
@@ -2,33 +2,44 @@ const chai = require("chai");
 const expect = chai.expect;
 
 const { Word } = require("../class");
+const { beforeEach } = require("mocha");
 
 describe("Word", function () {
+
+  let word;
+
+  beforeEach(() => {
+    word = new Word('testing');
+  });
+
   describe("Word constructor function", function () {
     it('should have a "word" property', function () {
-      expect.fail("replace with your code");
+      expect(word).to.have.property('word');
     });
-  
+
     it('should set the "word" property when a new word is created', function () {
-      expect.fail("replace with your code");
+      expect(word.word).to.equal('testing');
     });
   });
 
   describe("removeVowels function", function () {
     it("should return a the word with all vowels removed", function () {
-      expect.fail("replace with your code");
+      const removedVowels = word.removeVowels();
+      expect(removedVowels).to.equal('tstng');
     });
   });
 
   describe("removeConsonants function", function () {
     it("should return the word with the consonants removed", function () {
-      expect.fail("replace with your code");
+      const removedConsonants = word.removeConsonants();
+      expect(removedConsonants).to.equal('ei');
     });
   });
-  
+
   describe("pigLatin function", function () {
     it("should return the word converted to pig latin", function () {
-      expect.fail("replace with your code");
+      const latinWord = word.pigLatin();
+      expect(latinWord).to.equal('estingtay');
     });
   });
 });
